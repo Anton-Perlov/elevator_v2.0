0fdd45046ffb99c57b6c35c077c98b21e78b3801
Anton Perlov <anton.perlov@gmail.com>
2019-09-24 16:00:44 +0300

    2019-09-24 16:00

--- main.js
+++ main.js
@@ -5,20 +5,15 @@ let USERS_COUNT = 5;
 const FLOOR_HEIGHT = 50;
 const MOOVE_SPEED = 5;
 const USER_MOOVE_SPEED = 15;
-const CHANCE = 5;
+const CHANCE = 50;
 
 $(document).ready(function(){
     
     reInit();
     changeSettings();
-<<<<<<< HEAD
-
-=======
     
->>>>>>> 2f5052752fefe5acbc5e202788bb21c04cdf971d
     $('[data-toggle="tooltip"]').tooltip();
     
-
 });
     
 function reInit(){
@@ -28,5 +23,5 @@ function reInit(){
     createButtons();
     createUsers();
     userMove();
-    
+
 }
